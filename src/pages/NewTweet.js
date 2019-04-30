b48e1d949e13dfd32f599c843a8063650e5d10af
João Victor <albjoaov@gmail.com>
2019-04-30 00:02:16 -0300

    adding NewTweet view

--- src/pages/NewTweet.js
+++ src/pages/NewTweet.js
@@ -1,11 +1,73 @@
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { TextInput } from 'react-native-gesture-handler';
+
 
 // import { Container } from './styles';
 
 export default class NewTweet extends Component {
+
+  static navigationOptions = {
+    header: null
+  };
+
   render() {
-    return <Text>New</Text>;
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Icon name="close" size={24} color="#4BB0EE"/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Tweet</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TextInput
+          style={styles.input}
+          multiline
+          placeholder="What's happening?">
+        </TextInput>
+      </SafeAreaView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+
+  header: {
+    paddingTop: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  button: {
+    height: 32,
+    paddingHorizontal: 20,
+    borderRadius: 16,
+    backgroundColor: "#4BB0EE",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  buttonText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+
+  input: {
+    margin: 20,
+    fontSize: 16,
+    color: "#FFF"
+  }
+});
